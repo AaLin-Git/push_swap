25a37d23c2f575aa010941866d7668ea951795a0
Alona Kovalchuk <akovalch@f1r1s8.codam.nl>
2025-02-25 16:24:55 +0100

    create structure

---
+++ includes/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovalch <akovalch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 16:00:53 by akovalch          #+#    #+#             */
+/*   Updated: 2025/02/25 16:24:26 by akovalch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_node
+{
+	void			*content;
+	struct s_node	*next;
+}					t_node;
+
+#endif
