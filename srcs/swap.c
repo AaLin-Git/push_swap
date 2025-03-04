6cede3bdce953a40d1ba124f3aade54259c88698
Alona Kovalchuk <akovalch@f1r4s15.codam.nl>
2025-03-04 13:18:19 +0100

    swap works

--- srcs/swap.c
+++ srcs/swap.c
@@ -6,7 +6,7 @@
 /*   By: akovalch <akovalch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 11:34:30 by akovalch          #+#    #+#             */
-/*   Updated: 2025/03/04 12:58:37 by akovalch         ###   ########.fr       */
+/*   Updated: 2025/03/04 13:17:32 by akovalch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,20 @@ void swap(t_stack **stack)
 {
 	t_stack *first_node;
 	t_stack *second_node;
-	t_stack *third_node;
-	t_stack *last_node;
 
 	first_node = *stack;
 	second_node = first_node->next;
-	third_node = second_node->next;
-	last_node = first_node->prev;
+	if (second_node->next == first_node)
+		*stack = second_node;
 
-	second_node->next = first_node;
-	second_node->prev = last_node;
-	first_node->next = third_node;
+	first_node->prev->next = second_node;
+	second_node->next->prev = first_node;
+
+	first_node->next = second_node->next;
+	second_node->prev = first_node->prev;
+
 	first_node->prev = second_node;
-
-	last_node->next = second_node;
-	if(third_node)
-		third_node->prev = first_node;
+	second_node->next = first_node;
 
 	*stack = second_node;
 }
